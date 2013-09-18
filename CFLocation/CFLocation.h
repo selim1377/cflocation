a666afe9045c1291f9a423f14f2d14a03d8181c8
Selim Bakdemir <johannes@Johannes-Lumpes-MacBook-Pro-53.local>
2013-09-18 10:13:31 +0300

    Initial Commit

---
+++ CFLocation/CFLocation.h
@@ -0,0 +1,13 @@
+//
+//  CFLocation.h
+//  CFLocation
+//
+//  Created by Selim Bakdemir on 18.09.13.
+//  Copyright (c) 2013 Selim Bakdemir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFLocation : NSObject
+
+@end
---
+++ CFLocation/CFLocation.m
@@ -0,0 +1,13 @@
+//
+//  CFLocation.m
+//  CFLocation
+//
+//  Created by Selim Bakdemir on 18.09.13.
+//  Copyright (c) 2013 Selim Bakdemir. All rights reserved.
+//
+
+#import "CFLocation.h"
+
+@implementation CFLocation
+
+@end
